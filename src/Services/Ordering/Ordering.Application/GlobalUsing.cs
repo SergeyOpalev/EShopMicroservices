7ccec812b35eeb3d223e05e31c5234663bd880ef
Sergey Opalev <opalev@hotmail.co.uk>
2025-06-03 23:06:20 +0200

    Ordering.Application Layer with CQRS and MediatR

---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
+global using Ordering.Domain.Events;
+global using Ordering.Domain.Models;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Domain.ValueObjects.Ids;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<UpdateOrderCommand, UpdateOrderResult>
+{
+    public async Task<UpdateOrderResult> Handle(UpdateOrderCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = OrderId.Of(command.Order.Id);
+        var order = await dbContext.Orders.FindAsync([orderId], cancellationToken)
+            ?? throw new OrderNotFoundException(command.Order.Id);
+
+        UpdateOrderWithNewValues(order, command.Order);
+
+        dbContext.Orders.Update(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateOrderResult(true);
+    }
+
+    public static void UpdateOrderWithNewValues(Order order, OrderDto orderDto)
+    {
+        order.Update(
+            orderName: OrderName.Of(orderDto.OrderName),
+            shippingAddress: Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode),
+            billingAddress: Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode),
+            payment: Payment.Of(orderDto.Payment.CardName, orderDto.Payment.CardNumber, orderDto.Payment.Expiration, orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod),
+            status: orderDto.Status);
+    }
+}
